4bdfaa3508e1ea0eac3036de1a144ec02ee07dc6
mclehman <maxfieldlehman@gmail.com>
2015-09-26 00:34:51 -0700

    Add comments clarifying code
     - Adds comments to each method. Necessary because comments were initially nonexistent

--- P3/euler3.c
+++ P3/euler3.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <math.h>
 
+// Hardcode target number rather than deal with I/O
 #define  TARGET 600851475143
+// Assume maximum of 100 prime factors
 #define  MAXFACTORS 100
 
+// Define boolean type for clarity
 typedef int bool;
 #define true 1
 #define false 0
 
+// Determines if the requested number is prime
 bool isPrime(long n) {
+    // Check for 2 explicitly, as it is the only even prime
     if (n == 2) {
         return true;
     } else if (n % 2 == 0){// Check for divisiblity by two
         return false;
     } else {
+        // Check for divisibility by odd numbers, all even numbers already dealt with
         for (long div = 3; div <= (int)ceil(sqrt(n)); div += 2) {
+            // Return false if n is divisible by any number up to its square root
             if (n % div == 0) {
                 return false;
             }
@@ -23,11 +30,16 @@ bool isPrime(long n) {
     return true;
 }
 
+// Returns a pointer to an array of the requested numbers prime factors
 long * primeFactorsOf(long n) {
+    // Zero out entire array
     static long factors[MAXFACTORS] = {0};
     int index = 0;
+    // Loop through integers > 1 up to the square root of the number
     for (long div = 2; div <= (int)ceil(sqrt(n)); div++)
+        // check for divisibility and do not check primehood if unnecessary
         if ((n % div == 0) && isPrime(div)) {
+            // Add prime factors to array and increment index
             factors[index] = div;
             index++;
         }
@@ -36,11 +48,14 @@ long * primeFactorsOf(long n) {
     return factors;
 }
 
+// Returns the largest prime factor of requested number
 long largestPrimeFactor(long n) {
+    // Obtains pointer to array of prime factors
     long *factors = primeFactorsOf(n);
 
     long largest = factors[0];
     int index = 0;
+    // Loop through array and update largest as needed
     while(index < MAXFACTORS) {
         if (*(factors + index) > largest) {
             largest = *(factors + index);
@@ -51,6 +66,7 @@ long largestPrimeFactor(long n) {
     return largest;
 }
 
+// Prints out the largest prime factor of TARGET
 int main() {
     printf("%ld\n", largestPrimeFactor(TARGET));
     return 0;
