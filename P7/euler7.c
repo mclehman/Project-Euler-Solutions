d3003dd028589c56e233a26aa05b7498243c7fd3
mclehman <maxfieldlehman@gmail.com>
2015-09-26 00:56:46 -0700

    Comment, clarify, and optimize code
     - euler7.c was previously uncommented
    
     - Both even and odd numbers were checked for primehood. This shameful practice was ended.
    
     - Certain variable names were updated to clarify their purpose.

--- P7/euler7.c
+++ P7/euler7.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
+// Hardcode requested prime index
 #define TARGET 10001
 
+// Define boolean type for clarity
 typedef int   bool;
 #define true  1
 #define false 0
 
+// Determines if the requested number is prime
 bool isPrime(long n) {
-    //printf("Checking if %ld is prime\n", n);
+    // Checks for 2 explicitly, as it is the only even prime
     if (n == 2) {
         return true;
-    } else if (n % 2 == 0){
+    } else if (n % 2 == 0){ // Checks for divisiblity by two
         return false;
     } else {
+        // Checks for divisibility by odd numbers, all even numbers already dealt with
         for (long div = 3; div <= (int)ceil(sqrt(n)); div += 2) {
+            // Returns false if n is divisible by any number up to its square root
             if (n % div == 0) {
                 return false;
             }
@@ -23,22 +28,28 @@ bool isPrime(long n) {
     return true;
 }
 
+// Returns a pointer to an array of the first TARGET primes
 int * firstnPrimes() {
     static int primes[TARGET];
 
-    int i = 2;
-    int j = 0;
-    while(j < TARGET){
-        if (isPrime(i)) {
-            primes[j] = i;
-            j++;
+    primes[0] = 2; // Enters the only even prime to allow incrementing by 2 below
+
+    int possible = 3;
+    int index = 1;
+
+    // Checks odd numbers for primehood until TARGET many are found and stored in primes
+    while(index < TARGET){
+        if (isPrime(possible)) {
+            primes[index] = possible;
+            index++;
         }
-    i++;
+    possible += 2;
     }
 
     return primes;
 }
 
+// Prints the TARGET-th prime, conveniently located in the (TARGET - 1)-th index of primes.
 int main() {
     int *primes = firstnPrimes();
     printf("%d\n", *(primes + TARGET - 1));
