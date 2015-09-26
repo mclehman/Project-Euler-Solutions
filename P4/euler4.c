ee80b8efbef5a2cd24ac5c13d61b6baac2b7b728
mclehman <maxfieldlehman@gmail.com>
2015-09-26 00:46:49 -0700

    Add comments and remove debugging print statements
     - euler4.c was previously uncommented. PEBCAK suspected.
    
     - Debugging print statements were present due to a resolved bug.

--- P4/euler4.c
+++ P4/euler4.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
+// Define boolean type for clarity
 typedef int bool;
 #define true 1
 #define false 0
 
+// Returns the number of digits in the decimal representation of an integer
 int digitLength(int n) {
+    // One digit assumed until proven otherwise
     int result = 1;
     while(n > 9) {
+        // Divides n by 10 until a single digit number is reached, incrementing result each time
         n = n / 10;
         result++;
     }
@@ -15,13 +19,18 @@ int digitLength(int n) {
     return result;
 }
 
+// Returns true (1) if the decimal representation of the integer is a palindrome
 bool isPalindrome(int n) {
     int numDigits = digitLength(n);
-    int iDigit;
-    int iMirrorDigit;
+    int iDigit;       // Most signficant digit
+    int iMirrorDigit; // Least significant digit
+
     for (int i = numDigits; i > (numDigits / 2) - 1; i--) {
+        // iDigit is set to the i-th digit of n
         iDigit = (n % (int)pow(10, i)) / pow(10, i - 1);
+        // Its complement is set to the i-th digit from the left
         iMirrorDigit = (n % (int)pow(10, numDigits - i + 1)) / pow(10, numDigits - i);
+
         if (iDigit != iMirrorDigit) {
             return false;
         }
@@ -29,12 +38,14 @@ bool isPalindrome(int n) {
     return true;
 }
 
+// Returns the largest product of n-digit integers that is a palindrome
 int largestPalindromeProduct(int n) {
-    int minInt  = (int)(pow(10, n - 1));
-    int maxInt  = (int)(pow(10, n)) - 1;
+    int minInt  = (int)(pow(10, n - 1)); // 10^(n - 1) is the smallest n-digit integer
+    int maxInt  = (int)(pow(10, n)) - 1; // 10^(n) - 1 is the largest  n-digit integer
     int current;
-    int largest = 0;
+    int largest = 0; // return 0 if no palindrome product exists
 
+    // Loop through every possible combination of n-digit integers, updating the largest as needed
     for (int i = maxInt; i >= minInt; i--) {
         for (int j = maxInt; j >= minInt; j--) {
             current = i * j;
@@ -43,21 +54,12 @@ int largestPalindromeProduct(int n) {
             }
         }
     }
+
     return largest;
 }
 
 int main() {
-    int n = 3;
-
-    printf("Testing 1: %d\n", isPalindrome(1));
-    printf("Testing 13: %d\n", isPalindrome(13));
-    printf("Testing 131: %d\n", isPalindrome(131));
-    printf("Testing 133: %d\n", isPalindrome(133));
-    printf("Testing 1031: %d\n", isPalindrome(1031));
-    printf("Testing 1331: %d\n", isPalindrome(1331));
-    printf("Testing 13011: %d\n", isPalindrome(13011));
-    printf("Testing 11131: %d\n", isPalindrome(11131));
-    printf("Testing 10301: %d\n", isPalindrome(10301));
+    int n = 3; // Dimension requested by Project Euler question. Generalized for kicks
 
     printf("The largest palindrome product of %d-digit integers is: %d\n", n, largestPalindromeProduct(n));
     return 0;
